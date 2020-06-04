b9149d1009c34174bfa38ccd1b7dd88c289e72a0
sandeeppadala <spadala88>
2020-06-04 13:53:56 +0530

    test commit

--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                                         FirebaseDatabase database = FirebaseDatabase.getInstance();
                                         DatabaseReference myRef = database.getReference("message");
 
-                                        myRef.setValue("Hello, World sandy padala!");
+                                        myRef.setValue("Hello, World sandy padala hiiiiiii!");
                               }
                     });
           }
